ef24d30e5ebd2e9c6e6b04a31a3b24106c0bbdee
Hobr <mail@hobr.site>
2023-04-27 23:34:34 +0800

    int输入

--- script.py
+++ script.py
@@ -22,8 +22,9 @@ if config["init"] == 0:
     config["url"] = input(
         "请输入购票链接并按回车继续, 格式例如 https://show.bilibili.com/platform/detail.html?id=72320\n"
     )
-    config["count"] = input("请输入购票数量并按回车继续, 如 1\n")
-    config["process"] = input("请输入计划使用的进程数并按回车继续，如 1\n")
+    config["project_id"] = config["url"][50:55]
+    config["count"] = int(input("请输入购票数量并按回车继续, 如 1\n"))
+    config["process"] = int(input("请输入计划使用的进程数并按回车继续，如 1\n"))
     print("初始化成功\n")
     config["init"] = 1
     with open("./config.json", "w") as f:
@@ -43,105 +44,103 @@ def status(response, step):
 
 # 任务流
 def flow():
-    global globalStep
-    while globalStep <= 7:
-        # 0 登录
-        # 1 获取信息
-        # 获取 screen_id sku_id
-        config["project_id"] = config["url"][50:55]
-        print("1 获取信息")
-        url = "https://show.bilibili.com/api/ticket/project/get?version=134&id=" + config[
-            "project_id"]
-        response = requests.request("GET", url, headers=config["headers"])
-        status(response, 1)
-        data = response.json()
-        config["screen_id"] = data["data"]["ticket_list"]["id"]
-        config["sku_id"] = data["data"]["id"]
-        # 2 Token获取
-        # 需要 count project_id screen_id sku_id
-        # 获取 token
-        print("2 Token获取")
-        url = "https://show.bilibili.com/api/ticket/order/prepare"
-        payload = {
-            "count": config["count"],
-            "order_type": "1",
-            "project_id": config["project_id"],
-            "screen_id": config["screen_id"],
-            "sku_id": config["sku_id"],
-            "token": ""
-        }
-        response = requests.request("POST",
-                                    url,
-                                    headers=config["headers"],
-                                    json=payload)
-        status(response, 2)
-        data = response.json()
-        config["pay_money"] = data["data"]["pay_money"]
-        # 3 下单页信息
-        # 需要 token
-        # 获取 pay_money
-        print("3 下单页信息")
-        url = "https://show.bilibili.com/api/ticket/order/confirmInfo?token=" + config[
-            "token"] + "&voucher="
-        response = requests.request("GET", url, headers=config["headers"])
-        status(response, 3)
-        data = response.json()
-        config["token"] = data["data"]["token"]
-        # 4 付款人
-        # 需要 project_id
-        # 获取 buyer
-        print("4 付款人")
-        url = "https://show.bilibili.com/api/ticket/buyer/list?is_default&projectId=" + config[
-            "project_id"]
-        response = requests.request("GET", url, headers=config["headers"])
-        status(response, 4)
-        data = response.json()
-        config["buyer"] = data["data"]["list"]
-        # 5 创建订单
-        # 需要 buyer count pay_money project_id screen_id sku_id token
-        # 获取 order_token
-        print("5 创建订单")
-        url = "https://show.bilibili.com/api/ticket/order/createV2"
-        payload = {
-            "buyer_info": config["buyer"],
-            "count": config["count"],
-            "deviceId": "4aac6f1261084849aeef4d26bb79c59e",
-            "order_type": "1",
-            "pay_money": config["pay_money"],
-            "project_id": config["project_id"],
-            "screen_id": config["screen_id"],
-            "sku_id": config["sku_id"],
-            "timestamp": int(round(time.time() * 1000)),
-            "token": config["token"]
-        }
-        response = requests.request("POST",
-                                    url,
-                                    headers=config["headers"],
-                                    json=payload)
-        status(response, 5)
-        data = response.json()
-        config["order_token"] = data["data"]["token"]
-        # 6 支付信息
-        # 需要 order_token
-        # 获取 order_id
-        print("6 支付信息")
-        url = "https://show.bilibili.com/api/ticket/order/createstatus?token=" + config[
-            "order_token"] + "&timestamp=" + int(round(time.time() * 1000))
-        response = requests.request("GET", url, headers=config["headers"])
-        status(response, 6)
-        data = response.json()
-        config["token"] = data["data"]["order_id"]
-        # 7 订单状态
-        # 需要 order_id
-        print("7 订单状态")
-        url = "https://show.bilibili.com/api/ticket/order/info?order_id=" + config[
-            "order_id"] + "&timestamp=" + int(round(time.time() * 1000))
-        response = requests.request("GET", url, headers=config["headers"])
-        status(response, 7)
-        data = response.json()
-        print(data["data"]["status_name"])
-        with open("./config.json", "w") as f:
-            json.dump(config, f, indent=4)
+    # 0 登录
+    # 1 获取信息
+    # 获取 screen_id sku_id
+    config["project_id"] = config["url"][50:55]
+    print("1 获取信息")
+    url = "https://show.bilibili.com/api/ticket/project/get?version=134&id=" + config[
+        "project_id"]
+    response = requests.request("GET", url, headers=config["headers"])
+    status(response, 1)
+    data = response.json()
+    config["screen_id"] = data["data"]["ticket_list"]["id"]
+    config["sku_id"] = data["data"]["id"]
+    # 2 Token获取
+    # 需要 count project_id screen_id sku_id
+    # 获取 token
+    print("2 Token获取")
+    url = "https://show.bilibili.com/api/ticket/order/prepare"
+    payload = {
+        "count": config["count"],
+        "order_type": "1",
+        "project_id": config["project_id"],
+        "screen_id": config["screen_id"],
+        "sku_id": config["sku_id"],
+        "token": ""
+    }
+    response = requests.request("POST",
+                                url,
+                                headers=config["headers"],
+                                json=payload)
+    status(response, 2)
+    data = response.json()
+    config["pay_money"] = data["data"]["pay_money"]
+    # 3 下单页信息
+    # 需要 token
+    # 获取 pay_money
+    print("3 下单页信息")
+    url = "https://show.bilibili.com/api/ticket/order/confirmInfo?token=" + config[
+        "token"] + "&voucher="
+    response = requests.request("GET", url, headers=config["headers"])
+    status(response, 3)
+    data = response.json()
+    config["token"] = data["data"]["token"]
+    # 4 付款人
+    # 需要 project_id
+    # 获取 buyer
+    print("4 付款人")
+    url = "https://show.bilibili.com/api/ticket/buyer/list?is_default&projectId=" + config[
+        "project_id"]
+    response = requests.request("GET", url, headers=config["headers"])
+    status(response, 4)
+    data = response.json()
+    config["buyer"] = data["data"]["list"]
+    # 5 创建订单
+    # 需要 buyer count pay_money project_id screen_id sku_id token
+    # 获取 order_token
+    print("5 创建订单")
+    url = "https://show.bilibili.com/api/ticket/order/createV2"
+    payload = {
+        "buyer_info": config["buyer"],
+        "count": config["count"],
+        "deviceId": "4aac6f1261084849aeef4d26bb79c59e",
+        "order_type": "1",
+        "pay_money": config["pay_money"],
+        "project_id": config["project_id"],
+        "screen_id": config["screen_id"],
+        "sku_id": config["sku_id"],
+        "timestamp": int(round(time.time() * 1000)),
+        "token": config["token"]
+    }
+    response = requests.request("POST",
+                                url,
+                                headers=config["headers"],
+                                json=payload)
+    status(response, 5)
+    data = response.json()
+    config["order_token"] = data["data"]["token"]
+    # 6 支付信息
+    # 需要 order_token
+    # 获取 order_id
+    print("6 支付信息")
+    url = "https://show.bilibili.com/api/ticket/order/createstatus?token=" + config[
+        "order_token"] + "&timestamp=" + int(round(time.time() * 1000))
+    response = requests.request("GET", url, headers=config["headers"])
+    status(response, 6)
+    data = response.json()
+    config["token"] = data["data"]["order_id"]
+    # 7 订单状态
+    # 需要 order_id
+    print("7 订单状态")
+    url = "https://show.bilibili.com/api/ticket/order/info?order_id=" + config[
+        "order_id"] + "&timestamp=" + int(round(time.time() * 1000))
+    response = requests.request("GET", url, headers=config["headers"])
+    status(response, 7)
+    data = response.json()
+    print(data["data"]["status_name"])
+    with open("./config.json", "w") as f:
+        json.dump(config, f, indent=4)
 
 
 # 线程
